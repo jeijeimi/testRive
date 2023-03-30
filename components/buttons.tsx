c892fe66e4bc7e8f13e6701819bf11b9e60c6bab
Jeimi <floripowajaimez1@gmail.com>
2023-03-29 21:02:25 -0300

    new proyect test

--- components/buttons.tsx
+++ components/buttons.tsx
@@ -1,18 +1,17 @@
-import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.scss'
-import Button from 'components/buttons'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Hello there</title>
-      </Head>
-      <main>
-        <Button />
+      <main className='d-flex justify-content-center mt-5'>
+        <div className='Button'>
+          <div className='ButtonTxt'>
+            OK
+          </div>
+        </div>
       </main>
     </>
   )
